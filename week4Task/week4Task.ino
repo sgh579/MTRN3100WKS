401c8c5fbf3cafdb93cad2ccc31859ec1857af0e
sgh579 <henrysun579@outlook.com>
2025-06-23 10:43:56 +0800

    create the stub for task4

---
+++ week4Task/week4Task.ino
@@ -0,0 +1,50 @@
+#include "Encoder.hpp"
+#include "Motor.hpp"
+#include "PIDController.hpp"
+
+#define MOT1PWM 9 // PIN 9 is motor1's PWM pin
+#define MOT1DIR 10
+
+#define MOT2PWM ?// PIN ? is motor2's PWM pin
+#define MOT2DIR ?
+
+// TODO: create a struct for this, avoid using 2 separate motor objects
+mtrn3100::Motor motor1(MOT1PWM,MOT1DIR);
+mtrn3100::Motor motor2(MOT2PWM,MOT2DIR);
+
+#define EN1_A 2 // PIN 2 is encoder1's interupt
+#define EN1_B 4
+
+#define EN2_A  ?// PIN ? is encoder2's interupt
+#define EN2_B  ?
+
+mtrn3100::Encoder encoder1(EN1_A, EN1_B);
+mtrn3100::Encoder encoder2(EN2_A, EN2_B);
+
+// this implementation is a PID controller based on location
+// we need a new implementation that is unversial and can be used for speed control, path following, etc.
+mtrn3100::PIDController controller1(600, 1, 10); // Kp, Ki, Kd , motor1's position controller
+mtrn3100::PIDController controller2(600, 1, 10); // Kp, Ki, Kd , motor2's position controller
+
+void setup() {
+  Serial.begin(9600);
+
+  float target_motion_length = 1000; // 1000 mm, specified by task4
+  float motion_length_to_rotation_scale = 1; // to be adjusted based on the motor and encoder specifications
+  float target_motion_rotation_radians = target_motion_length * motion_length_to_rotation_scale * (2.0f * M_PIF / encoder1.counts_per_revolution);
+
+  controller1.zeroAndSetTarget(encoder1.getRotation(), target_motion_rotation_radians); 
+  controller2.zeroAndSetTarget(encoder2.getRotation(), target_motion_rotation_radians); 
+}
+
+void loop() {
+
+  int controller1_output = controller1.compute(encoder1.getRotation());
+  int controller2_output = controller2.compute(encoder2.getRotation());
+  
+  motor1.setPWM(controller_output1); 
+  motor2.setPWM(controller_output2); 
+
+  delay(50);  // loop interval, picked by random, to be adjusted based on the application requirements
+
+}
