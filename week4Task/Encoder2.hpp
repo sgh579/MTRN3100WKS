a14288de05272183f10448dc985c6b5da3bffd16
sgh579 <henrysun579@outlook.com>
2025-06-24 13:32:40 +1000

    pid and motor is working, para need adjusted

---
+++ week4Task/Encoder2.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <Arduino.h>
+
+// #define M_PIF 3.1415926f
+
+namespace mtrn3100 {
+
+
+// The encoder class is a simple interface which counts and stores an encoders count.
+// Encoder pin 1 is attached to the interupt on the arduino and used to trigger the count.
+// Encoder pin 2 is attached to any digital pin and used to derrive rotation direction.
+// The count is stored as a volatile variable due to the high frequency updates. 
+class Encoder2 {
+public:
+    Encoder2(uint8_t enc1, uint8_t enc2) : encoder1_pin(enc1), encoder2_pin(enc2) {
+        instance1 = this;  // Store the instance pointer
+        pinMode(encoder1_pin, INPUT_PULLUP);
+        pinMode(encoder2_pin, INPUT_PULLUP);
+
+        // attach the interrupt on pin one such that it calls the readEncoderISR function on a rising edg
+        attachInterrupt(digitalPinToInterrupt(encoder1_pin), readEncoderISR, RISING);
+
+
+    }
+
+
+    // Encoder function used to update the encoder
+    void readEncoder() {
+        noInterrupts();
+
+        // NOTE: DO NOT PLACE SERIAL PRINT STATEMENTS IN THIS FUNCTION
+        // NOTE: DO NOT CALL THIS FUNCTION MANUALLY IT WILL ONLY WORK IF CALLED BY THE INTERRUPT
+        // Increase or Decrease the count by one based on the reading on encoder pin 2
+        count += (digitalRead(encoder2_pin) == HIGH) ? -1 : 1;
+        interrupts();
+    }
+
+    // Helper function which to convert encouder count to radians
+    float getRotation() {
+        // return the current rotation in radians when called
+        return count * (2.0f * M_PIF / counts_per_revolution);
+    }
+
+    int getCount() {
+        // return the current count when called
+        return count;
+    }
+
+private:
+    static void readEncoderISR() {
+        if (instance1 != nullptr) {
+            instance1->readEncoder();
+        }
+    }
+
+public:
+    const uint8_t encoder1_pin;
+    const uint8_t encoder2_pin;
+    volatile int8_t direction;
+    float position = 0;
+    uint16_t counts_per_revolution = 700; 
+    volatile long count = 0;
+    uint32_t prev_time;
+    bool read = false;
+
+private:
+    static Encoder2* instance1;
+};
+
+Encoder2* Encoder2::instance1 = nullptr;
+
+}  // namespace mtrn3100
--- week4Task/week4Task.ino
+++ week4Task/week4Task.ino
@@ -1,4 +1,5 @@
 #include "Encoder.hpp"
+#include "Encoder2.hpp"
 #include "Motor.hpp"
 #include "PIDController.hpp"
 
@@ -19,12 +20,21 @@ mtrn3100::Motor motor2(MOT2PWM,MOT2DIR);
 #define EN2_B  8
 
 mtrn3100::Encoder encoder1(EN1_A, EN1_B);
-mtrn3100::Encoder encoder2(EN2_A, EN2_B);
+mtrn3100::Encoder2 encoder2(EN2_A, EN2_B);
 
 // this implementation is a PID controller based on location
 // we need a new implementation that is unversial and can be used for speed control, path following, etc.
-mtrn3100::PIDController controller1(600, 1, 10); // Kp, Ki, Kd , motor1's position controller
-mtrn3100::PIDController controller2(600, 1, 10); // Kp, Ki, Kd , motor2's position controller
+
+float kP_1 = 250;
+float kI_1 = 0;
+float kD_1 = 5;
+
+float kP_2 = 250;
+float kI_2 = 0;
+float kD_2 = 5;
+
+mtrn3100::PIDController controller1(kP_1, kI_1, kD_1); // Kp, Ki, Kd , motor1's position controller
+mtrn3100::PIDController controller2(kP_2, kI_2, kD_2); // Kp, Ki, Kd , motor2's position controller
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +44,7 @@ void setup() {
   float r = 15.5;
   float target_motion_rotation_radians = (target_motion_length * motion_length_to_rotation_scale * 700) / (2.0f * M_PIF * r ) ;
 
-  target_motion_rotation_radians = 100;
+  target_motion_rotation_radians = 2.0f * M_PIF;
 
   controller1.zeroAndSetTarget(encoder1.getRotation(), target_motion_rotation_radians); 
   controller2.zeroAndSetTarget(encoder2.getRotation(), -target_motion_rotation_radians); // reverse it for vehicle's motion
@@ -44,16 +54,18 @@ void loop() {
 
   int controller1_output = controller1.compute(encoder1.getRotation());
   int controller2_output = controller2.compute(encoder2.getRotation());
-  
   motor1.setPWM(controller1_output); 
   motor2.setPWM(controller2_output); 
 
   Serial.print("controller 1 output: ");
   Serial.println(controller1_output);
-
+  Serial.print("controller 2 output: ");
+  Serial.println(controller2_output);
   
   Serial.print("encoder1 count: ");
   Serial.println(encoder1.getCount());
+  Serial.print("encoder2 count: ");
+  Serial.println(encoder2.getCount());
 
 
   delay(50);  // loop interval, picked by random, to be adjusted based on the application requirements
