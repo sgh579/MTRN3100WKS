438c3a0661de981aa341b1a382c75542a615face
Naveen Thayyil <z5417679@ad.unsw.edu.au>
2025-07-14 12:24:51 +1000

    adding a file to incorporate StringToMotion.hpp

---
+++ Kinematic_control/StringToMotion.hpp
@@ -0,0 +1,65 @@
+//
+// Created by Naveen Thayyil on 13/7/2025.
+//
+
+//#ifndef ORIENTATIONFRAMEWORK_HPP
+//#define ORIENTATIONFRAMEWORK_HPP
+//
+//#endif //ORIENTATIONFRAMEWORK_HPP
+
+#pragma once
+
+#include <Arduino.h>
+
+String commandString = "";
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial) {}
+    Serial.println("Enter movement string:");
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        commandString = Serial.readStringUntil('\n');
+        commandString.trim();  // Remove trailing newline/space
+        Serial.print("Received command: ");
+        Serial.println(commandString);
+
+        executeCommands(commandString);
+    }
+}
+
+void executeCommands(String commands) {
+    for (int i = 0; i < commands.length(); i++) {
+        char c = commands.charAt(i);
+        switch (c) {
+            case 'f':
+                moveForwardOneCell();
+            break;
+            case 'l':
+                turnLeft90();
+            break;
+            case 'r':
+                turnRight90();
+            break;
+            default:
+                Serial.print("Invalid command: ");
+            Serial.println(c);
+            break;
+        }
+    }
+}
+
+// movement functions
+void moveForwardOneCell() {
+    float cellLength = 180.0f; // mm (example; measure it)
+    driveStraight(cellLength); // Your drive function
+}
+void turnLeft90() {
+    rotateInPlace(-90); // Negative for CCW
+}
+
+void turnRight90() {
+    rotateInPlace(90); // Positive for CW
+}
