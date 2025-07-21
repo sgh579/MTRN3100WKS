19f1e0a3cedc809330199fa58cc4f08a017663d6
sgh579 <henrysun579@outlook.com>
2025-07-21 13:45:54 +1000

    make variables named more accurately

--- Kinematic_control/Kinematic_control.ino
+++ Kinematic_control/Kinematic_control.ino
@@ -12,62 +12,60 @@
 #include <math.h> 
 
 
-
-#define EN_1_A 2 //These are the pins for the PCB encoder
-#define EN_1_B 7 //These are the pins for the PCB encoder
-#define EN_2_A 3 //These are the pins for the PCB encoder
-#define EN_2_B 8 //These are the pins for the PCB encoder
-#define MOT1PWM 11 // PIN 11 is motor1's PWM pin
+//These are the pins set up
+#define EN_1_A 2 
+#define EN_1_B 7 
+#define EN_2_A 3 
+#define EN_2_B 8 
+#define MOT1PWM 11 
 #define MOT1DIR 12
-#define MOT2PWM 9// PIN 9 is motor2's PWM pin
+#define MOT2PWM 9
 #define MOT2DIR 10
 
+// OLED display settings
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1
 #define SCREEN_ADDRESS 0x3C
 
-#define CELL_SIZE 180
-#define SIZE 8
+// Maze settings
+#define CELL_SIZE 180 // Size of the cell in mm, used for distance calculations
 
+// Global variables
+
+// Global objects
 mtrn3100::DualEncoder encoder(EN_1_A, EN_1_B,EN_2_A, EN_2_B);
 mtrn3100::EncoderOdometry encoder_odometry(15.5, 82); 
-
 mtrn3100::PIDController motor1_encoder_position_controller(100, 0.01, 0);
 mtrn3100::PIDController motor2_encoder_position_controller(100, 0.01, 0);
-
 mtrn3100::PIDController yaw_controller(0.05, 0.1, 0);
-
-// TODO: create a struct for this, avoid using 2 separate motor objects
 mtrn3100::Motor motor1(MOT1PWM,MOT1DIR);
 mtrn3100::Motor motor2(MOT2PWM,MOT2DIR);
-
-/*************** SCREEN *******************/
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 MPU6050 mpu(Wire);
 
-int loop_counter = 0;
+// Global variables
+int loop_counter = 0; // Counter for the loop iterations, used for debugging and control
 
-int cmd_ctr = 0;
-char commands[] = "frflflfr"; //[SIZE]
+int cmd_pointer = 0; // Pointer to the current command in the command sequence
+char commands[] = "frflflfr"; // Command sequence for the robot to follow
 
-float old_x = 0; 
-float old_y = 0; 
+float previous_X = 0; // Previous X position of the robot, used to calculate distance traveled between commands
+float previous_Y = 0; 
 
-float target_distance = 0; 
+float target_distance = 0; // Target for the robot to travel. Change this value and it applies in the feedbacj control loop
 float target_angle = 0;
 
-float current_angle_z;
+float current_angle_z; // Current angle of the robot in the Z direction, used for orientation control
 
-static float target_motion_rotation_radians = 0;
+float target_motion_rotation_radians = 0; // Target motion rotation in radians, calculated based on the target distance and robot specifications
 
-float curr_x = 0;
-float curr_y = 0;
+float curr_X = 0; // Current X position of the robot based on odometry, updated in each loop iteration
+float curr_Y = 0;
 float current_angle = 0;
 
-bool finished = false;
+// FLAGs
+bool cmd_sequence_completion_FLAG = false; 
 
 void setup() {
     Serial.begin(115200);
@@ -84,13 +82,12 @@ void setup() {
     mpu.calcOffsets(true,true);
     Serial.println("Done!\n");
 
-    // target_motion_rotation_radians = 2.0f * M_PIF;
 
-    motor1_encoder_position_controller.zeroAndSetTarget(encoder.getLeftRotation(), target_motion_rotation_radians); 
-    motor2_encoder_position_controller.zeroAndSetTarget(encoder.getRightRotation(), -target_motion_rotation_radians); // reverse it for vehicle's motion
-
+    // setup zero reference for the pid controllers
+    motor1_encoder_position_controller.zeroAndSetTarget(encoder.getLeftRotation(), 0); 
+    motor2_encoder_position_controller.zeroAndSetTarget(encoder.getRightRotation(), -0); // reverse it for vehicle's motion
     yaw_controller.zeroAndSetTarget(0, 0);
-    // yaw_controller.zeroAndSetTarget(current_angle_z, current_angle_z);
+
 
 }
 
@@ -103,35 +100,35 @@ void loop() {
     float current_angle_z = mpu.getAngleZ();
 
     // Take next instruction
-    curr_x = encoder_odometry.getX();
-    curr_y = encoder_odometry.getY();
+    curr_X = encoder_odometry.getX();
+    curr_Y = encoder_odometry.getY();
     current_angle = current_angle_z;
-    if (cmd_ctr == 0 || checkCompletedCommand()) {
+    if (cmd_pointer == 0 || checkCompletedCommand()) {
         // Stop if there are no more instructions 
-        // while (cmd_ctr >= commands.length()) {}
-        if (cmd_ctr >= sizeof(commands)-1) { // (cmd_ctr >= SIZE)
+        // while (cmd_pointer >= commands.length()) {}
+        if (cmd_pointer >= sizeof(commands)-1) { // (cmd_pointer >= SIZE)
             target_distance = 0; 
             target_angle = current_angle_z;
-            finished = true;
+            cmd_sequence_completion_FLAG = true;
         } else {
             // Record current state
-            old_x = encoder_odometry.getX();
-            old_y = encoder_odometry.getY();
+            previous_X = encoder_odometry.getX();
+            previous_Y = encoder_odometry.getY();
 
             // Set targets
-            char c = commands[cmd_ctr];
+            char c = commands[cmd_pointer];
             switch (c) {
                 case 'f':
                     target_distance = CELL_SIZE;
                     target_angle = current_angle_z;
-                    // yaw_controller.zeroAndSetTarget(0, 0); - removed
+                    // yaw_controller.zeroAndSetTarget(0, 0); - removed // TODO: dont ser target here, we can change the target angle value
                 break;
                 case 'l':
                     target_distance = 0;
                     // target_angle = (current_angle_z + 90) % 360;
                     target_angle = fmodf(current_angle_z + 90.0f, 360.0f);
                     if (target_angle < 0) target_angle += 360.0f;
-                    yaw_controller.zeroAndSetTarget(0, 90);
+                    yaw_controller.zeroAndSetTarget(0, 90); // TODO: dont use zero and se
                     // yaw_controller.setTarget(target_angle);
                 break;
                 case 'r':
@@ -142,13 +139,13 @@ void loop() {
                     // yaw_controller.setTarget(target_angle);
                 break;
                 default:
-                    Serial.print("Invalid command: ");
+                    Serial.print("Invalid command: "); // TODO maybe stop the loop
                     Serial.println(c);
                 break;
             }
 
-            cmd_ctr++;
-            if (cmd_ctr >= sizeof(commands) - 1) { // SIZE
+            cmd_pointer++;
+            if (cmd_pointer >= sizeof(commands) - 1) { // SIZE
     			motor1.setPWM(0);
     			motor2.setPWM(0);
                 // while (1);
@@ -202,7 +199,7 @@ void loop() {
     }
 
     // Stop the robot
-    while (finished) {}
+    while (cmd_sequence_completion_FLAG) {}
 
     delay(5); // TODO: REMOVE THIS MAYBE?
 
@@ -211,12 +208,12 @@ void loop() {
 // TODO: Checks if the current command has been completed
 // TODO: NEED TO CHECK WHAT THE RANGE FOR ANGLES IS e.g. 0 - 360
 bool checkCompletedCommand() {
-    char curr_cmd = commands[cmd_ctr - 1];
+    char curr_cmd = commands[cmd_pointer - 1];
 
     if (curr_cmd == 'f') {
         // TODO: MAY NEED TO ADJUST VARIANCE
-        float delta_x = curr_x - old_x;
-        float delta_y = curr_y - old_y; 
+        float delta_x = curr_X - previous_X;
+        float delta_y = curr_Y - previous_Y; 
 
         return sqrt(pow(delta_x, 2) + pow(delta_y, 2)) >= CELL_SIZE;
     } else if (curr_cmd == 'l') {
